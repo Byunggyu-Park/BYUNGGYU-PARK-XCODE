3341557376e133c89aeeb682d2e8aa575f7ae6c3
Park <park.byunggyu@icloud.com>
2022-05-10 03:43:36 -0700

    Initial Commit

---
+++ source_control_from/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  source_control_from
+//
+//  Created by Byunggyu Park on 2022/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ source_control_from/source_control_fromApp.swift
@@ -0,0 +1,17 @@
+//
+//  source_control_fromApp.swift
+//  source_control_from
+//
+//  Created by Byunggyu Park on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct source_control_fromApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
